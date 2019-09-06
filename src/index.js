fb67e498e63486eec0905c7eda6621c0a5d4d471
Daniel Lwetabe <daniel.lwetabe@andela.com>
2019-09-06 20:07:52 +0300

    boostrap application

--- src/index.js
+++ src/index.js
@@ -6,9 +6,50 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('ummm...we are live\n');
-})
+let users = {
+    1: {
+      id: '1',
+      username: 'Robin Wieruch',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+  };
+
+  let messages = {
+    1: {
+      id: '1',
+      text: 'Hello World',
+      userId: '1',
+    },
+    2: {
+      id: '2',
+      text: 'By World',
+      userId: '2',
+    },
+  };
+
+app.get('/users', (req, res) => {
+    return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+    return res.send(users[req.params.id]);
+});
+
+app.post('/users', (req, res) => {
+    return res.send('ummm...we are live\n');
+});
+
+app.put('/users/:userId', (req, res) => {
+    return res.send(`PUT methond on user/ ${req.params.userId}`);
+});
+
+app.delete('/users/userId', (req, res) => {
+    return res.send(`DELETE methond on user/ ${req.params.userId}`);
+});
+
 app.listen(process.env.PORT, () =>
   console.log(`listening on port ${process.env.PORT}!`),
 );
