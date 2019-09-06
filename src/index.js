47ad8a37deb33bbf9932efbb1bf7ff6d3ca1548e
Daniel Lwetabe <daniel.lwetabe@andela.com>
2019-09-06 18:58:52 +0300

    added CORS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('ummm...we are live\n');
 })
-app.listen(3000, () =>
-  console.log('listening on port 3000!'),
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port ${process.env.PORT}!`),
 );
