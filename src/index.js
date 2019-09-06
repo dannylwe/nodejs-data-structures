e183a3854107f9d26174df9b85e69f47ee58756b
Daniel Lwetabe <daniel.lwetabe@andela.com>
2019-09-06 18:55:10 +0300

    initial endpoint working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('ummm...we are live\n');
+})
 app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+  console.log('listening on port 3000!'),
 );
